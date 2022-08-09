f7b5be825129d2c59a0eaac06dde3b47b60cf479
jsbmomo <tytyes40@gmail.com>
2022-08-09 23:59:50 +0900

    Java Reflection을 활용한 클래스나 필드 객체 정보 파악하기

--- src/main/java/part_3/JavaField.java
+++ src/main/java/part_3/JavaField.java
@@ -2,17 +2,32 @@ package part_3;
 
 import java.lang.reflect.Field;
 
+/**
+ * Java Reflection을 통해 실행 시, 클래스나 필드 객체의 정보를 동적으로 가져올 수 있다.
+ * 이 기능으로 객체 타입을 미리 알 수 없거나 프로그램이 실행되기 전까지 존재하지 않는
+ * 제네릭 알고리즘과 라이브러리를 작성할 수 있다.
+ * 또한, 실행 시 클래스 필드를 검사하는 동안 Java 컴파일러가 숨겨놓은 작업을 확인할 수 있다.
+ */
 public class JavaField {
 
-  public static void main(String[] args) {
-    printDeclaredFieldsInfo(Movie.class);
+  public static void main(String[] args) throws NoSuchFieldException {
+//    printDeclaredFieldsInfo(Movie.class);
+//    printDeclaredFieldsInfo(Movie.MovieStats.class);
+    Movie movie = new Movie("Lord of the Rings", 2001, 12.99, true, Category.ADVENTURE);
+    // printDeclaredFieldsInfo(movie.getClass(), movie);
+
+    Field minPriceStaticField = Movie.class.getDeclaredField("MINIMUM_PRICE");
+    System.out.println(String.format("static MINIMUM_PRICE value : $f", minPriceStaticField));
   }
 
-  public static void printDeclaredFieldsInfo(Class<?> clazz) {
+  public static <T> void printDeclaredFieldsInfo(Class<? extends  T> clazz, T instance) throws IllegalAccessException {
     for (Field field : clazz.getDeclaredFields()) {
       System.out.println(String.format("Field name : %s type : %s",
           field.getName(),
           field.getType().getName()));
+
+      System.out.println(String.format("Is synthetic field : %s", field.isSynthetic()));
+      System.out.println(String.format("Field value is %s", field.get(instance)));
 
       System.out.println();
     }
@@ -30,6 +45,19 @@ public class JavaField {
       this.isReleased = isReleased;
       this.category = category;
       this.actualPrice = Math.max(price, MININUM_PRICE);
+    }
+
+    // Nested class
+    public class MovieStats {
+      private double timesWatched;
+
+      public MovieStats(double timesWatched) {
+        this.timesWatched = timesWatched;
+      }
+
+      public double getRevenue() {
+        return timesWatched * actualPrice;
+      }
     }
   }
 
